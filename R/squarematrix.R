76b25d13a04af5d854a371a10e34548f0f5e5ce6
Sven E. Templer <sven.templer@gmail.com>
2014-06-06 09:05:43 +0200

    improved checks, simplified code and updated documentation

--- R/squarematrix.R
+++ R/squarematrix.R
@@ -1,29 +1,31 @@
-#' @name squarematrix
-#' @keywords square matrix
-#' @author Sven E. Templer (\email{sven.templer@@gmail.com})
-#' @title Create a Square Matrix
-#' @description
-#' Transform any m x n (m != n) matrix to a square matrix by column/row names.
+#' Create a Square Matrix
+#' 
+#' Transform any m x n matrix to a square matrix by column/row names.
+#' Stops if no or duplicated dimnames are provided in x.
+#' 
 #' @param x Object of class \code{matrix}.
 #' @return
 #' Returns a \code{matrix}.
 #' @examples
-#' m <- matrix(1:6,2)
-#' dimnames(m) <- list(2:3,1:3)
+#' #
+#' 
+#' m <- matrix(1:6, 2, dimnames=list(2:3,1:3))
 #' m
 #' squarematrix(m)
+#' 
+#' #
+#' @name squarematrix
+#' @keywords square matrix
+#' @author Sven E. Templer (\email{sven.templer@@gmail.com})
 
 #' @export squarematrix
 squarematrix <- function (x) {
-  
-  if (is.null(dimnames(x))) { stop("Provide dimnames for matrix x.")}
-  
-  n <- unique(unlist(dimnames(x)))
+  if (is.null(dimnames(x)))
+    stop("Provide dimnames for matrix x.")
+  if (any(sapply(dimnames(x), function (i) any(duplicated(i)))))
+    stop("Duplicated dimnames found!")
+  n <- sort(unique(unlist(dimnames(x))))
   o <- matrix(NA, nrow = length(n), ncol = length(n), dimnames = list(n, n))
-  
   o[rownames(x), colnames(x)] <- x
-  o <- o[sort(rownames(o)), sort(colnames(o))]
-  
   return(o)
-  
 }
