98ad23b3f0b41f90efeceeac34fe9e42a51a2f47
Sven E. Templer <sven.templer@gmail.com>
2014-06-06 10:13:23 +0200

    added function textable

---
+++ R/textable.R
@@ -0,0 +1,57 @@
+#' Table to Latex
+#' 
+#' This function enhances \code{xtable} as it writes the latex code to a file
+#' and adds a header/footer for the document structure to allow a direct
+#' creation of a pdf with for example pdflatex.
+#' 
+#' @param d Object to transform to a latex table.
+#' @param file Character string with output file name.
+#' @param caption Character vector with title of table.
+#' @param rownames Logical, include row names of \code{d}.
+#' @param landscape Logical, use a landscape format for wider tables.
+#' @param pt.size Integer from 10 to 13 for the size of the characters.
+#' @param margin Margin between table and page border in cm.
+#' @param digits Number of digits to print from numeric values.
+#' @param align Character vector with \code{'l'}, \code{'c'}, \code{'r'}
+#' for aligning the columns left, centered or right. Length is either one
+#' or 1 (for rownames column) + number of columns in \code{d} (even if
+#' \code{rownames = FALSE})
+#' @param label Character vector with the latex label or HTML anchor.
+#' @return
+#' Creates a file at the path given in \code{file} containting the latex code
+#' with the table and document structure for an A4 latex article.
+#' @seealso
+#' \link{xtable} 
+#' @examples
+#' # NA
+#' @name textable
+#' @importFrom xtable xtable
+#' @author Sven E. Templer (\email{sven.templer@@gmail.com})
+
+
+textable <- function (d, file, caption = NULL, rownames = FALSE, landscape = FALSE,
+                      pt.size = 10, margin = 2, digits = NULL, align = NULL,
+                      label = NULL)
+{
+	if (!pt.size %in% 10:12)
+		stop('pt.size must be 10, 11 or 12.')
+	if (!is.null(align) && length(align) == 1)
+		align <- rep(align, ncol(d)+1)
+  x <- capture.output(print(xtable(
+    d, digits=digits, align=align, label=label, caption=caption),
+    include.rownames = rownames))
+  tex.landscape <- ''
+  if (landscape)
+		tex.landscape <- 'landscape,'
+	tex <- paste0(
+		'\\documentclass[a4paper,', pt.size,
+		'pt]{article}\n\\usepackage[a4paper,',
+		tex.landscape,
+		'margin=',
+		margin,
+		'cm]{geometry}\n\\begin{document}\n') #\\small\n
+  cat(tex, file=file)
+	cat(x, file=file, append=T, sep='\n')
+	cat('\\end{document}\n', file=file, append=T)
+  invisible(NULL)
+}
