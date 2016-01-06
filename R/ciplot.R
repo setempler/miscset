e3bb17b7ceabe99adb3c8994589246536b8cfab3
Sven E. Templer <sven.templer@gmail.com>
2016-01-06 08:28:25 +0100

    ciplot fixes

--- R/ciplot.R
+++ R/ciplot.R
@@ -1,4 +1,5 @@
 #' @name ciplot
+#' @keywords barplot error bars
 #' @author Sven E. Templer
 #' @title Barplot with confindence intervals
 #' @description 
@@ -11,6 +12,7 @@
 #' @param range.fun Function to calculate the range of the
 #' arrows (e.g. a confidence interval). See details.
 #' @details 
+#' Use standard deviation to calculate range:\cr
 #' sd_range <- function (x) { m <- mean(x); s <- sd(x); c(m-s,m+s)}
 
 
@@ -21,10 +23,25 @@ ciplot <- function (x, ...) { UseMethod("ciplot") }
 
 #' @rdname ciplot
 #' @export
-ciplot.default <- function (x, height.fun = mean, range.fun = ci, ..., arrows.args = list()) {
+ciplot.default <- function (x, height.fun = mean, range.fun = confint, ylim,
+                            ..., arrows.args = list()) {
+  
   heights <- sapply(x, height.fun)
-  arrows <- lapply(x, range.fun)
-  bars <- barplot(heights, ...)
-  arrows.args <- list(x0 = bars[,1])
+  arrows.r <- lapply(x, range.fun)
+  arrows.left <- sapply(arrows.r, min)
+  arrows.right <- sapply(arrows.r, max)
+  if (missing(ylim)) {
+    al <- min(0, arrows.left)
+    al <- ifelse(al < 0, al * 1.1, al * 0.9)
+    ar <- max(0, arrows.right * 1.1)
+    ylim <- c(al, ar)
+  }
+    
+  bars <- barplot(heights, ylim = ylim, ...)
+  arrows.x <- sapply(bars, as.vector)
+  arrows.args <- c(list(x0 = arrows.x, x1 = arrows.x, y0 = arrows.left, y1 = arrows.right, 
+                        code = 3, angle = 90), arrows.args)
+  do.call(arrows, arrows.args)
+  invisible(list(heights, arrows.r))
 }
 
