a8c6df808b5a3487302b1b4ccf1c0394f0146b1d
Sven E. Templer <sven.templer@gmail.com>
2016-04-20 09:09:46 +0200

    better info() printing

--- R/info.R
+++ R/info.R
@@ -21,14 +21,21 @@ info <- function(..., width = 120) {
   libs <- unique(normalizePath(.libPaths(), winslash = "/"))
   nums <- sapply(libs, function (x) length(list.files(x)))
   rbin <- suppressMessages(system('which R', intern = TRUE))
+  repo <- getOption("repos")
+
   dsys <- data.frame(setup = c("R binary"), value = rbin)
+  drep <- data.frame(repository = names(repo), url = repo)
   dlib <- data.frame(library = libs, packages = nums, stringsAsFactors = FALSE)
+  # getOp
 
   cat("\n")
   print(session_info(...))
   cat("\n")
   devtools:::rule("Directories")
   print(dsys, right = FALSE, row.names = FALSE)
+  cat("\n")
+  print(drep, right = FALSE, row.names = FALSE)
+  cat("\n")
   print(dlib, right = FALSE, row.names = FALSE)
   cat("\n")
   options(width = oldwidth)
